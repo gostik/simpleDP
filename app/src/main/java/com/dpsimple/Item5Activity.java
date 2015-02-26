5fb40e0c69289f5fa85d60ecddc9e34882e2744c
Zisman Mikhail <mzisman@exadel.com>
2015-02-26 11:11:49 +0500

    rework

---
+++ app/src/main/java/com/dpsimple/Item5Activity.java
@@ -0,0 +1,29 @@
+package com.dpsimple;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+/**
+ * @author MZ
+ *
+ */
+public class Item5Activity extends BaseActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		/**
+		 * Adding our layout to parent class frame layout.
+		 */
+		getLayoutInflater().inflate(R.layout.activity_main, frameLayout);
+		
+		/**
+		 * Setting title and itemChecked  
+		 */
+		mDrawerList.setItemChecked(position, true);
+		setTitle(listArray[position]);
+		
+		((ImageView)findViewById(R.id.image_view)).setBackgroundResource(R.drawable.image5);
+	}
+}
