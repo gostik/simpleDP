c6bb7dc9fbfb23e20812690586abe2b09fc9dad4
Zisman Mikhail <mzisman@exadel.com>
2015-03-04 22:53:23 +0500

    C:\Projects\AndroidProjects\Dp.simple\app\src\main\java\com\dpsimple\NotSwipableViewPager.java

---
+++ app/src/main/java/com/dpsimple/NotSwipableViewPager.java
@@ -0,0 +1,40 @@
+package com.dpsimple;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+public class NotSwipableViewPager extends ViewPager {
+    private boolean mIsEnabledSwipe = true;
+
+    public NotSwipableViewPager(Context context) {
+        super(context);
+    }
+
+    public NotSwipableViewPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (!mIsEnabledSwipe) {
+            return false;
+        }
+
+        return super.onTouchEvent(event);
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent event) {
+        if (!mIsEnabledSwipe) {
+            return false;
+        }
+        return super.onInterceptTouchEvent(event);
+    }
+
+    public void setEnabledSwipe(boolean enabled) {
+        mIsEnabledSwipe = enabled;
+    }
+
+}
